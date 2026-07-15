e038e632d209d6ac31d8ffb4bc93564eab9d89ed
OmidProgramm <omidvarisl@gmx.de>
2026-07-15 12:28:55 +0200

    feat: test change password-type

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
+import { FiEye } from "react-icons/fi";
+import { FiEyeOff } from "react-icons/fi";
 import './App.css'
 
 function App() {
-  
+  const [type,setType] = useState("password")
+  const [display,setDisplay] = useState(true)
+  const changeType = ()=>{
+    setType(type === "password" ? "text" : "password");
+    setDisplay(!display);
+  }
   return (
     <>
       <h1 className='projTitle'>Hello React</h1>
-      <input type='password' placeholder='password'/>
+      <input type={type} placeholder='password'/>
+      <FiEye style={{ display: display ? "inline" : "none" }}
+      onClick={changeType}/>
+      <FiEyeOff style={{ display: display ? "none" : "inline" }}
+      onClick={changeType}/>
     </>
   )
 }
