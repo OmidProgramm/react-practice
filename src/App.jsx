19893eaebff9556a4b8625e643c867e796be8036
OmidProgramm <omidvarisl@gmx.de>
2026-07-15 11:32:05 +0200

    install: react-icons & ready to test

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [name, setName] = useState(["one","two","three"]);
-  const [item,setItem] = useState("");
-
-  const changeName = (e)=>{
-    e.preventDefault(); 
-    setName([...name,item])
-    setItem("")
-  }
   
   return (
     <>
-      <h1>Hello React</h1>
-      <form>
-        <label htmlFor="fname">First Name</label>
-        <input type="text" id="fname" name="fname" placeholder="Your name.." value={item} onChange={(e)=>setItem(e.target.value)}/>
-        <button className="btn" onClick={changeName}>add</button>
-      </form>  
-      <ul>
-        {
-          name.map((elem,index)=>{
-            return <li key={index}>{elem}</li>
-          })
-        }
-      </ul>
+      <h1 className='projTitle'>Hello React</h1>
+      <input type='password' placeholder='password'/>
     </>
   )
 }
