7a0307efee668acbf9f48f2a20b561ebbf735270
OmidProgramm <omidvarisl@gmx.de>
2026-07-16 21:23:49 +0200

    feat: fetch data from API

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,20 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [products, setProducts] = useState([]);
+  const getData = ()=>{
+    fetch("https://fakestoreapi.com/products")
+    .then(data=>data.json())
+    .then(res=>setProducts(res))
+    .catch(err=>console.log(err))
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+  console.log(products)
   return (
     <>
-      
+      <h4>Hello Fetch</h4>
     </>
   )
 }
