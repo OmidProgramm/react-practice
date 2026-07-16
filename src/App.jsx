3fe459976592f118d38448c2a84675d5c606f73e
OmidProgramm <omidvarisl@gmx.de>
2026-07-16 22:16:08 +0200

    change: fetch to async

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,10 @@ import './App.css'
 
 function App() {
   const [products, setProducts] = useState([]);
-  const getData = ()=>{
-    fetch("https://fakestoreapi.com/products")
-    .then(data=>data.json())
-    .then(res=>setProducts(res))
-    .catch(err=>console.log(err))
+  const getData = async ()=>{
+    let data = await fetch("https://fakestoreapi.com/products")
+    let res = await data.json()
+    setProducts(res)
   }
   useEffect(()=>{
     getData()
