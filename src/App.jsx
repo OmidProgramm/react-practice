29f6427ad20f6ab7b0f15740c1dab5b7da3d7559
OmidProgramm <omidvarisl@gmx.de>
2026-07-16 21:06:41 +0200

    feat: clear component for API-TEST

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import { useState } from 'react'
-import { FiEye } from "react-icons/fi";
-import { FiEyeOff } from "react-icons/fi";
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [type,setType] = useState("password")
-  const [display,setDisplay] = useState(true)
-  const changeType = ()=>{
-    setType(type === "password" ? "text" : "password");
-    setDisplay(!display);
-  }
+  
   return (
     <>
-      <h1 className='projTitle'>Hello React</h1>
-      <input type={type} placeholder='password'/>
-      <FiEye style={{ display: display ? "inline" : "none" }}
-      onClick={changeType}/>
-      <FiEyeOff style={{ display: display ? "none" : "inline" }}
-      onClick={changeType}/>
+      
     </>
   )
 }
