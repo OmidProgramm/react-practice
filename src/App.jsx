165a559f77c284b61704c66057f8b3a5c73e9c73
OmidProgramm <omidvarisl@gmx.de>
2026-07-19 10:30:38 +0200

    delete: delete all components for next practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,11 @@
 import { useEffect, useState } from 'react'
-import Products from './components/Products/Products';
 
 import './App.css'
-import Navbars from './components/Navbar/Navbars';
-import Sliders from './components/Sliders/Sliders';
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [menu, setMenu] = useState([]);
-  const [slider, setSlider] = useState([]);
-  const getProducts = async ()=>{
-    try {
-    let data = await fetch("http://localhost:3006/products")
-    let res = await data.json()
-    setProducts(res)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  const getMenu = async ()=>{
-    try {
-    let data = await fetch("http://localhost:3006/menu")
-    let res = await data.json()
-    setMenu(res)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  const getSlider = async ()=>{
-    try {
-    let data = await fetch("http://localhost:3006/slider")
-    let res = await data.json()
-    setSlider(res)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  useEffect(()=>{
-    getMenu()
-    getProducts()
-    getSlider()
-  },[])
-  
   return (
     <>
-    <Navbars menu={menu}/>
-      <Sliders slider={slider}/>
-      <Products products={products}/>
+    <h4>Hello</h4>
     </>
   )
 }
--- src/components/Navbar/NavItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NavItem = ({link}) => {
-  return (
-    <li><a href={link.link}>{link.name}</a></li>
-  )
-}
-
-export default NavItem
