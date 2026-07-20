4326c384118803eaa3045b07aa7d955d2d73275b
OmidProgramm <omidvarisl@gmx.de>
2026-07-20 10:05:43 +0200

    feat: finish tst jspdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,66 @@
 import React, { useEffect, useState } from 'react'
-
+import { jsPDF } from 'jspdf'
+import autoTable from "jspdf-autotable";
 import './App.css';
-
 
 
 function App() {
   const [products, setProducts] = useState(null)
   const getProducts = async ()=>{
-  let data = await fetch("https://fakestoreapi.com/products")
-  let res = await data.json()
-  setProducts(res)
- 
-}
-useEffect(()=>{
-  getProducts()
-},[])
-   console.log(products)
+    let data = await fetch("https://fakestoreapi.com/products")
+    let res = await data.json()
+    setProducts(res)
+  }
+  const generatePdf = ()=>{
+    const pdf = new jsPDF();
+    autoTable(pdf, {
+      html: "#table",
+    });
+    pdf.save("test.pdf");
+
+  }
+
+
+  useEffect(()=>{
+    getProducts()
+  },[])
+   
   return (
     <>
     <h4>App</h4>
-    
     <div>
+      <button onClick={generatePdf}>print</button>
+    </div>
+    
+    <table id='table'>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Title</th>
+          <th>Price</th>
+          <th>Category</th>
+          <th>Count</th>
+        </tr>
+      </thead>
+      <tbody>
       {
         products && products.map(product=>{
           return(
-            <div key={product.id} >
-              <h5>{product.title}</h5>
-              <h5>{product.price}</h5>
-              <h5>{product.category}</h5>
-              <h5>{product.rating.count}</h5>
-              <hr/>
-            </div>
+            <tr key={product.id} >
+              <td>{product.id}</td>
+              <td>{product.title}</td>
+              <td>{product.price}</td>
+              <td>{product.category}</td>
+              <td>{product.rating.count}</td>
+              
+            </tr>
           )
         })
       }
-    </div>
+      </tbody>
+    </table>
 
     </>
-      
-    
   )
 }
 
