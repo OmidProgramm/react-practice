4db3120e320280fa5f75905ac84f5e10c1978fd2
OmidProgramm <omidvarisl@gmx.de>
2026-07-20 11:41:48 +0200

    finish: test skeleton loading

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import  "./Products.css"
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export const Product = ({product}) => {
-    console.log(product)
+     const [title, setTitle] = useState('')
+    const [price, setPrice] = useState('')
+    const [category, setCategory] = useState('')
+    const [description, setDescription] = useState('') 
+    const [image, setImage] = useState('') 
+    useEffect(()=>{
+        setTimeout(()=>{
+            setTitle(product.title)
+            setCategory(product.category)
+            setPrice(product.price)
+            setDescription(product.description)
+            setImage(product.image)
+        },1000)
+    },[])
   return (
     <div className='product'>
-        <h4 className="title">{product.title}</h4>
-        <p>{product.price}</p>
-        <p>{product.description.substr(0,50)}</p>
-        <div className='imageParent'><img src={product.image}/></div>
+        <h4 className="title">{title || <Skeleton count={1} />} </h4>
+        <h4 className="title">{category || <Skeleton count={1} /> }</h4>
+        <div className='imageParent'><img src={image || <Skeleton count={4} /> }/></div>
+        <p>Price: {price || <Skeleton count={1} /> }</p>
+        <p>{description.substr(0,50) || <Skeleton count={2} /> }</p>
     </div>
   )
 }
