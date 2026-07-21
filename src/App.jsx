648fcbc248948557d1b3c0ac1d45ce7e183398c5
OmidProgramm <omidvarisl@gmx.de>
2026-07-21 15:13:50 +0200

    feat: clean App

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,10 @@ import Button from './components/Button/Button';
 
 
 function App() {
-  const [counter1,setCounter1] = useState(0)
-  const [counter2,setCounter2] = useState(0)
-
-  const changeCounter1 = ()=>{
-    setCounter1(counter1 + 1)
-    
-  }
-  const changeCounter2 = ()=>{
-    setCounter2(counter2 + 1)
-  }
-  const isEven = useMemo(()=>{
-    let i =0
-    while(i <30000000000){
-      i++
-    }
-    return counter1 %2 === 0
-  },[counter1])
+ 
   return (
     <>
-    <Navbars menu={menu}/>
-      <Sliders slider={slider}/>
-      <Products products={products}/>
+    
     </>
   )
 }
