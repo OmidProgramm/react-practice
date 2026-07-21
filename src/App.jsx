b397172b3c511489f64a94de8cb816eb1cc82e3a
OmidProgramm <omidvarisl@gmx.de>
2026-07-21 15:10:18 +0200

    finish: test useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useReducer, useState } from 'react'
+import  { useCallback, useMemo, useState } from 'react'
 import './App.css';
 import Title from './components/Title/Title';
 import Counter from './components/Counter/Counter';
@@ -9,25 +9,30 @@ function App() {
   const [counter1,setCounter1] = useState(0)
   const [counter2,setCounter2] = useState(0)
 
-  const changeCounter1 = useCallback(()=>{
+  const changeCounter1 = ()=>{
     setCounter1(counter1 + 1)
     
-  },[counter1])
-
-  const changeCounter2 = useCallback(()=>{
+  }
+  const changeCounter2 = ()=>{
     setCounter2(counter2 + 1)
-  },[counter2])
+  }
+  const isEven = useMemo(()=>{
+    let i =0
+    while(i <30000000000){
+      i++
+    }
+    return counter1 %2 === 0
+  },[counter1])
   return (
     <>
       <h4>App</h4>
-      <Title/>
+     
       <div className="parentbtn">
-         <Counter text={'1'} counter={counter1}/>
-        <Button text={'1'} changeCounter={changeCounter1}/>
-        <Counter text={'2'} counter={counter2}/>
-        <Button text={'2'} changeCounter={changeCounter2}/>
+         <button className='btn' onClick={changeCounter1}>+</button>
+         <span>{counter1} - {isEven?" even": " odd"} </span>
+         <button className='btn' onClick={changeCounter2}>+</button>
+         <span>{counter2}</span>
        </div>
-      
      </>
   )
 }
